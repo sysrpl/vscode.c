6b18495fb816f0ff6795cbe5865670118d541fe3
Anthony Walter <sysrpl@gmail.com>
2023-10-13 00:45:49 -0400

    Formatted code

--- gcc-gtk3-app/exampleappprefs.h
+++ gcc-gtk3-app/exampleappprefs.h
@@ -1,15 +1,12 @@
 #ifndef __EXAMPLEAPPPREFS_H
 #define __EXAMPLEAPPPREFS_H
 
-#include <gtk/gtk.h>
 #include "exampleappwin.h"
+#include <gtk/gtk.h>
 
+#define EXAMPLE_APP_PREFS_TYPE (example_app_prefs_get_type())
+G_DECLARE_FINAL_TYPE(ExampleAppPrefs, example_app_prefs, EXAMPLE, APP_PREFS, GtkDialog)
 
-#define EXAMPLE_APP_PREFS_TYPE (example_app_prefs_get_type ())
-G_DECLARE_FINAL_TYPE (ExampleAppPrefs, example_app_prefs, EXAMPLE, APP_PREFS, GtkDialog)
-
-
-ExampleAppPrefs        *example_app_prefs_new          (ExampleAppWindow *win);
-
+ExampleAppPrefs *example_app_prefs_new(ExampleAppWindow *win);
 
 #endif /* __EXAMPLEAPPPREFS_H */
