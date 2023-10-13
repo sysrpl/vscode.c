4b58a0e744d25e914cd3b66bfa80bc79444cf3b7
Anthony Walter <sysrpl@gmail.com>
2023-10-13 00:46:48 -0400

    Formatted code

--- gcc-gtk3-app/exampleapp.h
+++ gcc-gtk3-app/exampleapp.h
@@ -3,12 +3,9 @@
 
 #include <gtk/gtk.h>
 
+#define EXAMPLE_APP_TYPE (example_app_get_type())
+G_DECLARE_FINAL_TYPE(ExampleApp, example_app, EXAMPLE, APP, GtkApplication)
 
-#define EXAMPLE_APP_TYPE (example_app_get_type ())
-G_DECLARE_FINAL_TYPE (ExampleApp, example_app, EXAMPLE, APP, GtkApplication)
-
-
-ExampleApp     *example_app_new         (void);
-
+ExampleApp *example_app_new(void);
 
 #endif /* __EXAMPLEAPP_H */
